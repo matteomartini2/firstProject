353434b29486277a72888550d9c25c87dcf1a1f5
Emanuele <manuros00@gmail.com>
2022-12-07 17:08:16 +0100

    toolbar

--- firstProjectAngular/src/app/app.module.ts
+++ firstProjectAngular/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule,MatButtonModule, BrowserAnimationsModule
+    MatSlideToggleModule,MatButtonModule, BrowserAnimationsModule,MatCardModule,MatToolbarModule,MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
